b640da89cfa133dc32256f4206b5eadc1109c2da
Angelique CD <angelcd@stanford.edu>
2025-02-15 07:59:58 -0800

    added navbar to main layout

---
+++ app-react/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/images/logo.jpg';
+import '../../theme_files/styles.css';
+
+const Navbar = () => {
+  const linkClass = ({ isActive }) =>
+    isActive
+      ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+      : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
+  return (
+    <nav className='bg-indigo-700 border-b border-indigo-500'>
+      <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+        <div className='flex h-20 items-center justify-between'>
+          <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
+            <NavLink className='flex flex-shrink-0 items-center mr-4' to='/'>
+              <img className='h-10 w-auto' src={logo} alt='React Jobs' />
+              <span className='hidden md:block text-white text-2xl font-bold ml-2'>
+                E-Z Read
+              </span>
+            </NavLink>
+            <div className='md:ml-auto'>
+              <div className='flex space-x-2'>
+                <NavLink to='/' className={linkClass}>
+                  Home
+                </NavLink>
+                <NavLink to='/upload' className={linkClass}>
+                  Upload
+                </NavLink>
+                <NavLink to='/text' className={linkClass}>
+                  Text
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
