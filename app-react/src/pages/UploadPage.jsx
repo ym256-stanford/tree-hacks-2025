f62674db744eca10d936a3280a6c0c7994f26a63
Angelique CD <angelcd@stanford.edu>
2025-02-15 07:38:53 -0800

    added router and skeletons for pages

--- app-react/src/pages/UploadPage.jsx
+++ app-react/src/pages/UploadPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Outlet} from 'react-router-dom'
 
 const UploadPage = () => {
   return (
+    <>
+    <Outlet/>
     <div>
       Upload your file here.
     </div>
+    </>
   )
 };
 
