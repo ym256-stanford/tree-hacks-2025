da42508c36be8d2304cb329e37659ad8adfbd04f
Angelique CD <angelcd@stanford.edu>
2025-02-14 21:50:24 -0800

    initialized folder and added page skeletons

---
+++ app-react/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      Reading has never been easier!
+    </div>
+  )
+}
+
+export default HomePage
---
+++ app-react/src/pages/UploadPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UploadPage = () => {
+  return (
+    <div>
+      Upload your file here.
+    </div>
+  )
+};
+
+export default UploadPage;
